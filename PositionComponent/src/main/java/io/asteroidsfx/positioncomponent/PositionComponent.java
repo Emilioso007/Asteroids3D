118033f05558f9c13d0a5b9c1982df1a9ad18201
Emil Jørgensen <emiljorgensen05@gmail.com>
2026-02-21 16:49:57 +0100

    add PositionComponent module

---
+++ PositionComponent/src/main/java/io/asteroidsfx/positioncomponent/PositionComponent.java
@@ -0,0 +1,7 @@
+package io.asteroidsfx.positioncomponent;
+
+import io.asteroidsfx.common.Component;
+
+public class PositionComponent extends Component {
+    public double x, y;
+}
---
+++ PositionComponent/src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module PositionComponent {
+    requires Common;
+}
