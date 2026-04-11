005c88533f391e31b32b3d6848e3cd5cacbba3f5
Emil Jørgensen <emiljorgensen05@gmail.com>
2026-04-11 09:35:44 +0200

    add Quaternion and Vector3D classes for 3D vector and quaternion operations

---
+++ Common/Common/src/main/java/io/asteroidsjaylib/common/util/Quaternion.java
@@ -0,0 +1,73 @@
+package io.asteroidsjaylib.common.util;
+
+import com.raylib.Raylib;
+
+public class Quaternion {
+
+    public float x, y, z, w;
+
+    public Quaternion(){
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+        this.w = 1;
+    }
+
+    public Quaternion(float x, float y, float z, float w){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
+    }
+
+    public Quaternion(Raylib.Vector4 vector4){
+        this(vector4.x(), vector4.y(), vector4.z(), vector4.w());
+    }
+
+    public Raylib.Vector4 toVector4(){
+        return new Raylib.Vector4().x(x).y(y).z(z).w(w);
+    }
+
+    public Quaternion copy(){
+        return new Quaternion(x, y, z, w);
+    }
+
+    public static Quaternion fromAxisAngle(Vector3D axis, float angle){
+        Vector3D normAxis = axis.copy().normalize();
+        float halfAngle = angle/2.0f;
+        float sinHalf = (float) Math.sin(halfAngle);
+
+        return new Quaternion(
+            normAxis.x * sinHalf,
+            normAxis.y * sinHalf,
+            normAxis.z * sinHalf,
+            (float) Math.cos(halfAngle)
+        );
+    }
+
+    public Quaternion multiply(Quaternion other){
+        float newX = this.w * other.x + this.x * other.w + this.y * other.z - this.z * other.y;
+        float newY = this.w * other.y - this.x * other.z + this.y * other.w + this.z * other.x;
+        float newZ = this.w * other.z + this.x * other.y - this.y * other.x + this.z * other.w;
+        float newW = this.w * other.w - this.x * other.x - this.y * other.y - this.z * other.z;
+
+        this.x = newX;
+        this.y = newY;
+        this.z = newZ;
+        this.w = newW;
+        return this;
+    }
+
+    public Vector3D rotateVector(Vector3D v){
+        Vector3D qVec = new Vector3D(this.x, this.y, this.z);
+
+        Vector3D t = qVec.cross(v).mult(2.0f);
+
+        Vector3D part1 = v.copy();
+        Vector3D part2 = t.copy().mult(this.w);
+        Vector3D part3 = qVec.cross(t);
+
+        return part1.add(part2).add(part3);
+    }
+
+}
